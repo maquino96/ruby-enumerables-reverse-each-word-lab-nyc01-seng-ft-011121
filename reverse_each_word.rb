fb313fae06088279a4599651435d3fa68260e8ba
Matthew Aquino <matthewaquino96@gmail.com>
2021-01-04 02:30:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,8 @@
 def reverse_each_word (str)
   
   array = str.split
-  new_array = []
-  array.each do |el|
-      new_array.push(el.reverse)
+  new_array = array.collect do |el|
+      el.reverse
   end
   new_array.join(" ")
 end 
