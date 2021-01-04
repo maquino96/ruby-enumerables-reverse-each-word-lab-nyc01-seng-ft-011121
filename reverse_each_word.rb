3fc70dff110f614e7d717ae27f7449aa24b698d6
Matthew Aquino <matthewaquino96@gmail.com>
2021-01-04 02:26:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word (str)
   array = str.split
   result=""
   array.each do |el|
-      result = "#{result} #{el.reverse}" 
+      result = result+ " #{el.reverse}" 
   end
   result
 end 
